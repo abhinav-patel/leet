467ecd6776cb891b85a53ba2791d5fdae91056c3
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2024-02-21 09:50:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (96.74%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -3,22 +3,19 @@ class Solution {
     {
         int a=0;
         int b=0;
-        int c=0;
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]==0)
                 a++;
             else if(nums[i]==1)
                 b++;
-            else
-                c++;
         }
         int i=0;
         for(;i<a;i++)
             nums[i]=0;
         for(;i<a+b;i++)
             nums[i]=1;
-        for(;i<a+b+c;i++)
+        for(;i<nums.length;i++)
             nums[i]=2;           
     }
 }
