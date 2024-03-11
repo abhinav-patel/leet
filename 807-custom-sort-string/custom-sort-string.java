5566c97bb695893a79659eb6f80c848f2026547e
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2024-03-11 11:19:16 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (98.09%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -1,37 +1,35 @@
-class Solution {
+class Solution 
+{
     public String customSortString(String order, String s) 
     {
-        int count[]=new int[26];
-        for(int i=0;i<s.length();i++)
+        int count[] = new int[26];
+        for (int i = 0; i < s.length(); i++) 
         {
-            ++count[(int)(s.charAt(i)-'a')];
+            ++count[(int) (s.charAt(i) - 'a')];
         }
 
-        StringBuilder result=new StringBuilder();
-        for(int i=0;i<order.length();i++)
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < order.length(); i++) 
         {
-            char ch=order.charAt(i);
-            int idx=(int)(ch-'a');
-            int k=count[idx];
-            while(count[idx]-->0)
+            char ch = order.charAt(i);
+            int idx = (int) (ch - 'a');
+            int k = count[idx];
+            while (count[idx]-- > 0)
                 result.append(ch);
         }
         for (int i = 0; i < 26; ++i) 
         {
-        if (count[i] > 0) {
-          char ch = (char)(i + 'a');
-          for (int j = 0; j < count[i]; ++j) {
-            result.append(ch);
-          }
-        }
-      }
-      return result.toString();
-            
-            
+            if (count[i] > 0) 
+            {
+                char ch = (char) (i + 'a');
+                for (int j = 0; j < count[i]; ++j) 
+                {
+                    result.append(ch);
+                }
+            }
 
         }
+        return result.toString();
+    }
 
-
-
-        
 }
