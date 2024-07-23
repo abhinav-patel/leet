155c46cadab557bc281828e0574e98762b99d3b8
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2024-07-24 02:04:16 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (94.59%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -1,12 +1,40 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
-	Map<Integer, Integer> map = new HashMap<>();
-	// count frequency of each number
-	Arrays.stream(nums).forEach(n -> map.put(n, map.getOrDefault(n, 0) + 1));
-	// custom sort
-	return Arrays.stream(nums).boxed()
-			.sorted((a,b) -> map.get(a) != map.get(b) ? map.get(a) - map.get(b) : b - a)
-			.mapToInt(n -> n)
-			.toArray();
-}
+        int[] count=new int[202];
+        for(int i=0;i<nums.length;i++){
+            count[nums[i]+100]++;
+        }
+        quickSort(nums,count,0,nums.length-1);
+        return nums;
+    }
+    void quickSort(int[] nums,int freq[],int low,int high){
+        if(low<high){
+            int pivot=partition(nums,freq,low,high);
+            quickSort(nums,freq,low,pivot-1);
+            quickSort(nums,freq,pivot+1,high);
+        }
+    }
+    int partition(int[] nums,int freq[],int low,int high){
+        int pivot=freq[nums[high]+100];
+        int i=low-1;
+        for(int j=low;j<high;j++){
+            if(pivot > freq[nums[j]+100]){
+                i++;
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+            }else if(freq[nums[j]+100]==pivot){
+                if(nums[high]<=nums[j]){
+                    i++;
+                    int temp=nums[j];
+                    nums[j]=nums[i];
+                    nums[i]=temp;
+                }
+            }
+        }
+        int temp=nums[i+1];
+        nums[i+1]=nums[high];
+        nums[high]=temp;
+        return i+1;
+    }
 }
