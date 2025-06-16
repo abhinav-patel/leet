515cbc1a9ca2edb2691c584fe9f9865d4ec8c888
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2025-06-16 18:30:41 +0530

    Time: 5 ms (57.48%) | Memory: 42.6 MB (98.93%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -5,16 +5,15 @@ class Solution {
            return false;
 
         int m1[]=new int[26];
-        int m2[]=new int[26];
 
         for(int i=0;i<s.length();i++)
         {
             m1[s.charAt(i)-'a']++;
-            m2[t.charAt(i)-'a']++;
+            m1[t.charAt(i)-'a']--;
         }
         for(int i=0;i<26;i++)
         {
-            if(m1[i]!=m2[i])
+            if(m1[i]!=0)
                return false;
         }
         return true;
