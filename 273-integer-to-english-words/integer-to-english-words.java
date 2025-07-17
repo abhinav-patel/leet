2187e45deb0e6153441ec4e46537d6f477079d90
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2025-07-17 11:26:31 +0530

    Time: 1 ms (100.00%) | Memory: 42.1 MB (73.16%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.java
+++ 273-integer-to-english-words/integer-to-english-words.java
@@ -1,29 +1,33 @@
 class Solution 
 {
-    
-    String ones[]={"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
-    String tens[]={"","Ten","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
-    
+    int values[]= {1000000000, 1000000, 1000, 100,90, 80, 70, 60, 50, 40, 30, 20,19, 18, 17, 16, 15, 14, 13, 12, 11, 10,9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+    String words[] = {"Billion", "Million", "Thousand", "Hundred","Ninety", "Eighty", "Seventy", "Sixty", "Fifty","Forty", "Thirty", "Twenty","Nineteen", "Eighteen", "Seventeen", "Sixteen", "Fifteen","Fourteen", "Thirteen", "Twelve", "Eleven", "Ten","Nine", "Eight", "Seven", "Six", "Five", "Four", "Three","Two", "One" };
+
     public String numberToWords(int num) 
     {
-        if(num==0)
-           return "Zero";
-        return helper(num).trim();  
+        if (num == 0)
+            return "Zero";
+        return helper(num).trim();
     }
 
-    public String helper(int num)
+    public String helper(int num) 
     {
-        if(num>=1000000000)
-           return (helper(num/1000000000)+" Billion "+ helper(num%1000000000));
-        if(num>=1000000)
-           return (helper(num/1000000)+" Million "+helper(num%1000000));
-        if(num>=1000)
-           return (helper(num/1000)+" Thousand "+helper(num%1000));
-        if(num>=100)
-           return (helper(num/100)+" Hundred "+(helper(num%100))).trim();
-        if(num>=20)
-           return (tens[num/10]+" "+helper(num%10)).trim();
-        return ones[num];
-        
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=0;i<values.length;i++)
+        {
+            int x=values[i];
+            if(num>=x)
+            {
+                if(num>=100)
+                   sb.append(helper(num/x)).append(" ");
+                sb.append(words[i]);
+                if(num%x>0)
+                   sb.append(" ").append(helper(num%x));
+                break;
+            }
+        }
+        return sb.toString();
     }
 }
