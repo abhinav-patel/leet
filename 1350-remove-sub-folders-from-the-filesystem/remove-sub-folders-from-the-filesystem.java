7524d17d3a8f6d1c5a970477921ebe7516f3ddae
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2025-07-19 19:40:51 +0530

    Time: 261 ms (5.08%) | Memory: 56.5 MB (37.70%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<String> removeSubfolders(String[] folder) 
     {
-        Arrays.sort(folder);
+        /*Arrays.sort(folder);
         List<String> res=new ArrayList<>();
         res.add(folder[0]);
 
@@ -11,6 +11,27 @@ class Solution {
             if(!folder[i].startsWith(curr))
                res.add(folder[i]);
         }
-        return res; 
+        return res; */
+
+        Set<String> set=new HashSet<>();
+        for(String str:folder)
+        {
+            set.add(str);
+        }
+        List<String> res=new ArrayList<>();
+
+        for(String str:folder)
+        {
+            res.add(str);
+            for(int i=1;i<str.length();i++)
+            {
+                if(str.charAt(i)=='/' && set.contains(str.substring(0,i)))
+                {
+                    res.remove(str);
+                    break;
+                }
+            }
+        }
+        return res;
     }
 }
