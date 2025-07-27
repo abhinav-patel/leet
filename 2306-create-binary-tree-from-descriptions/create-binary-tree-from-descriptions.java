1f604ff1c25b909d1d90eb5c32d3b5c842d916c3
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2025-07-27 14:11:23 +0530

    Time: 7 ms (100.00%) | Memory: 55.3 MB (91.48%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
@@ -16,7 +16,47 @@
 class Solution {
     public TreeNode createBinaryTree(int[][] descriptions) 
     {
-        HashMap<Integer,TreeNode> map=new HashMap<>();
+        //to measure dimension of the array
+        int max=0;
+        for(int arr[]:descriptions)
+        {
+            max=Math.max(max,Math.max(arr[0],arr[1]));
+        }
+
+        //to put all the children only on the map;
+        TreeNode map[]=new TreeNode[max+1];
+        for(int arr[]:descriptions)
+        {
+            map[arr[1]]=new TreeNode(arr[1]);
+        }
+        
+        //to find only parent that is not in the map and it will be the root node and make it root
+        TreeNode root=null;
+        for(int arr[]:descriptions)
+        {
+            if(map[arr[0]]==null)
+            {
+                map[arr[0]]=new TreeNode(arr[0]);
+                root=map[arr[0]];
+                break;
+            }
+        }
+
+        //to assign the lefts and rights
+        for(int arr[]:descriptions)
+        {
+            if(arr[2]==1)
+               map[arr[0]].left=map[arr[1]];
+            else
+               map[arr[0]].right=map[arr[1]];       
+        }
+
+        return root;
+
+
+
+
+        /*HashMap<Integer,TreeNode> map=new HashMap<>();
         HashSet<Integer> children=new HashSet<>();
 
         for(int arr[]:descriptions)
@@ -45,5 +85,6 @@ class Solution {
             }
         }
         return null;
+        */
     }
 }
