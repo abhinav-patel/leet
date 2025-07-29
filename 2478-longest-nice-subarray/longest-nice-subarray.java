b24d780fd7e251320fd901ccd1c1e9ae345d3535
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2025-07-29 13:28:11 +0530

    Time: 3 ms (99.15%) | Memory: 56.4 MB (5.54%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.java
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -1,17 +1,33 @@
 class Solution {
     public int longestNiceSubarray(int[] nums) 
     {
+        // int res=0;
+        // int left=0;
+        // int and=0;
+        // for(int right=0;right<nums.length;right++)
+        // {
+        //     while((and & nums[right])>0)
+        //        and^=nums[left++];
+        //     and|=nums[right];
+        //     res=Math.max(res,right-left+1);
+        // }
+        // return res;
+
+        int start=0,end=0;
         int res=0;
-        int left=0;
-        int and=0;
-        for(int right=0;right<nums.length;right++)
+        int n=nums.length;
+
+        for( end=0;end<n;end++)
         {
-            while((and & nums[right])>0)
-               and^=nums[left++];
-            and|=nums[right];
-            res=Math.max(res,right-left+1);
+            for(int i=start;i<end;i++)
+            {
+                if((nums[i]&nums[end])!=0)
+                {
+                    start=i+1;
+                }
+            }
+            res=Math.max(res,end-start+1);
         }
-        return res;
-        
+    return res;
     }
 }
