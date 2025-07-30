f6f5d434524dbee8b85aec6a6ddcc153cb96e512
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2025-07-30 12:40:04 +0530

    Time: 3 ms (99.96%) | Memory: 58.2 MB (13.78%) - LeetSync

--- 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.java
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.java
@@ -1,18 +1,53 @@
-class Solution {
+class Solution 
+{
     public int partitionArray(int[] nums, int k) 
     {
-        Arrays.sort(nums);
-        int c=1;
-        int min=nums[0];
-        for(int i=1;i<nums.length;i++)
+        //int n=nums.length;
+        int max=-1;
+        int min=Integer.MAX_VALUE;
+
+        for(int n:nums)
         {
-            if(nums[i]-min>k)
-            {
-                c++;
-                min=nums[i];
-            }
+            if(n>max)
+               max=n;
+            if(n<min)
+               min=n;  
+        }
+        if(max-min<=k)
+           return 1;
+        
+        boolean temp[]=new boolean[max+1];
+        for(int n:nums)
+           temp[n]=true;
+
+        int c=1;
+        for(int i=min+k+1;i<=max;)
+        {
+            while(!temp[i++])
+            {}
+            c++;
+            i=i+k;
         }
         return c;
+
+        
+
+
+
+
+
+        // Arrays.sort(nums);
+        // int c=1;
+        // int min=nums[0];
+        // for(int i=1;i<nums.length;i++)
+        // {
+        //     if(nums[i]-min>k)
+        //     {
+        //         c++;
+        //         min=nums[i];
+        //     }
+        // }
+        // return c;
         
     }
 }
