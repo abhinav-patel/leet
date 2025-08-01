96c1a13141c0ff27d7c133a4fcd749ec8dae5990
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2025-08-01 09:47:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (91.00%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,10 +1,5 @@
-class Solution {
-    public List<List<Integer>> generate(int n) 
-    {
-        List<List<Integer>> res=new ArrayList<>();
-        helper(res,0,n);
-        return res;
-    }
+class Solution 
+{
     public void helper(List<List<Integer>> res,int i,int n)
     {
         List<Integer> temp=new ArrayList<>();
@@ -26,4 +21,31 @@ class Solution {
         res.add(temp);
         helper(res,i+1,n);
     }
+    public List<List<Integer>> generate(int n) 
+    {
+        List<List<Integer>> res=new ArrayList<>();
+        helper(res,0,n);
+        return res;
+    }
+    // public void helper(List<List<Integer>> res,int i,int n)
+    // {
+    //     List<Integer> temp=new ArrayList<>();
+    //     if(i>=n)
+    //        return;
+    //     else if(i==0)
+    //     {
+    //         temp.add(1);
+    //     }
+    //     else
+    //     {
+    //         temp.add(1);
+    //         for(int j=1;j<i;j++)
+    //         {
+    //             temp.add(res.get(i-1).get(j-1)+res.get(i-1).get(j));
+    //         }
+    //         temp.add(1);
+    //     }
+    //     res.add(temp);
+    //     helper(res,i+1,n);
+    // }
 }
