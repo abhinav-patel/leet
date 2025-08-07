5c5c2c2e81cfa1b7422ca20e80d38f2970ac9861
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2025-08-07 16:31:09 +0530

    Time: 31 ms (50.00%) | Memory: 198.3 MB (30.00%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.java
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.java
@@ -1,81 +1,64 @@
-class Solution {
+class Solution 
+{
+    int memo[][];
     int n;
-    int[][] memo;
-
-    // row = i, col = j
-    int dfs3(int row, int col, int[][] mat) {
-        if (row < 0 || col < 0 || row >= n || col >= n) return 0;
-
-        if (memo[row][col] != -1) return memo[row][col];
-
-        int val = mat[row][col];
-        int res = 0;
-
-        if (row == col) {
-            res = Math.max(res, dfs3(row + 1, col + 1, mat));
-        } else if (row - 1 == col) {
-            res = Math.max(res, Math.max(
-                dfs3(row + 1, col + 1, mat),
-                dfs3(row, col + 1, mat)
-            ));
-        } else {
-            res = Math.max(res, Math.max(
-                dfs3(row + 1, col + 1, mat),
-                Math.max(dfs3(row, col + 1, mat),
-                         dfs3(row - 1, col + 1, mat))
-            ));
+    public int maxCollectedFruits(int[][] fruits) 
+    {
+        int res=0;
+        n=fruits.length;
+        memo=new int[n][n];
+        for(int i=0;i<n;i++)
+        {
+            res+=fruits[i][i];
+            Arrays.fill(memo[i],-1);
+            fruits[i][i]=0;
         }
-
-        return memo[row][col] = val + res;
+        
+        res=res+dfs2(0,n-1,fruits)+dfs3(n-1,0,fruits);
+        return res;
     }
 
-    int dfs2(int row, int col, int[][] mat) {
-        if (row < 0 || col < 0 || row >= n || col >= n) return 0;
+    public int dfs2(int i, int j, int fruits[][])
+    {
+        if(i<0||j<0|| i>=n || j>=n)
+            return 0;
+        if(memo[i][j]!=-1)
+           return memo[i][j];
 
-        if (memo[row][col] != -1) return memo[row][col];
+        int val=fruits[i][j];
+        int res=0;
 
-        int val = mat[row][col];
-        int res = 0;
+        if(i==j)
+           res=Math.max(res,dfs2(i+1,j+1,fruits));
+        else if(i==j-1)
+           res=Math.max(res,Math.max(dfs2(i+1,j,fruits),dfs2(i+1,j+1,fruits)));
+                                    //South              //South east
+        else
+           res=Math.max(res,Math.max(dfs2(i+1,j-1,fruits),Math.max(dfs2(i+1,j,fruits),dfs2(i+1,j+1,fruits))));
+                                     //South-west                  //South            //South-East
 
-        if (row == col) {
-            res = Math.max(res, dfs2(row + 1, col + 1, mat));
-        } else if (row == col - 1) {
-            res = Math.max(res, Math.max(
-                dfs2(row + 1, col + 1, mat),
-                dfs2(row + 1, col, mat)
-            ));
-        } else {
-            res = Math.max(res, Math.max(
-                dfs2(row + 1, col + 1, mat),
-                Math.max(dfs2(row + 1, col, mat),
-                         dfs2(row + 1, col - 1, mat))
-            ));
-        }
-
-        return memo[row][col] = val + res;
+        return memo[i][j]=res+val;
     }
 
-    public int maxCollectedFruits(int[][] mat) {
-        n = mat.length;
-        int total = 0;
+    public int dfs3(int i, int j, int fruits[][])
+    {
+        if(i>=n ||i<0||j<0|| j>=n)
+           return 0;
+        
+        if(memo[i][j]!=-1)
+           return memo[i][j];
 
-        memo = new int[n][n];
-        for (int[] row : memo) {
-            Arrays.fill(row, -1);
-        }
+        int val=fruits[i][j];
+        int res=0;
 
-        // child - 1
-        // he will eat all diagonal fruits, so set them to 0 
-        for (int i = 0; i < n; i++) {
-            total += mat[i][i];
-            mat[i][i] = 0;
-        }
+        if(i==j)
+           res=Math.max(res,dfs3(i+1,j+1,fruits));
+                                 
+        else if(i-1==j)
+           res=Math.max(res,Math.max(dfs3(i,j+1,fruits),dfs3(i+1,j+1,fruits)));
+        else
+           res=Math.max(res,Math.max(dfs3(i-1,j+1,fruits),Math.max(dfs3(i,j+1,fruits),dfs3(i+1,j+1,fruits))));
 
-        // child - 2
-        total += dfs3(n - 1, 0, mat);
-        // child - 3
-        total += dfs2(0, n - 1, mat);
-
-        return total;
+        return memo[i][j]=res+val;
     }
 }
