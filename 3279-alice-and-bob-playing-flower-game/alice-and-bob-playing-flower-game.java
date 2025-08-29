a3bb1a9393feaab88015651fa32f829680312212
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2025-08-29 19:05:44 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (40.00%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
@@ -1,6 +1,9 @@
 class Solution {
     public long flowerGame(int n, int m) 
     {
+//         The outcome depends only on whether the numbers of flowers are odd or even. Alice wins if one lane has an odd number of flowers and the other lane has an even number.
+
+// There are n * m total possible pairs. Exactly half of them will have different parity (one odd, one even). Therefore the answer is n * m / 2.
         long c=((long)n*m)/2;
         return c;
     }
