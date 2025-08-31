4200cdeedb1e0caafbfb748da0f312564a79684d
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2025-08-31 07:05:24 +0530

    Time: 1 ms (99.87%) | Memory: 44.5 MB (56.61%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,16 +1,17 @@
-class Solution {
-    public boolean isValidSudoku(char[][] board) 
+class Solution 
+{
+    public static boolean isValidSudoku(char[][] board) 
     {
         boolean rows[][]=new boolean[10][10];
         boolean columns[][]=new boolean[10][10];
 
         boolean boxs[][]=new boolean[9][10];
 
-
         for(int i=0;i<9;i++)
         {
             for(int j=0;j<9;j++)
             {
+                //for rows and columns
                 char ch=board[i][j];
                 if(ch=='.')
                    continue;
@@ -20,9 +21,11 @@ class Solution {
                 rows[i][k]=true;
                 columns[j][k]=true;
 
+                //for cecking the sub boxes
                 int subrow=i/3;
                 int subcolumn=j/3;
                 int subIndex=(subrow*3)+subcolumn;
+                //formula to calculate the subIndex for the boxes from 0 to 8
                 if(boxs[subIndex][k]==true)
                    return false;
                 boxs[subIndex][k]=true;
