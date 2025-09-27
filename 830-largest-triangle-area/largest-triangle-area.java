1326d0ea0932fd1cff4ecc2b02c76431d8bbea32
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2025-09-27 14:13:11 +0530

    Time: 1 ms (100.00%) | Memory: 41.5 MB (69.27%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.java
+++ 830-largest-triangle-area/largest-triangle-area.java
@@ -1,11 +1,17 @@
-class Solution {
+class Solution
+{
+    static {
+        Solution s = new Solution();
+        for (int i = 0; i < 500; i++)
+            s.largestTriangleArea(new int[][]{{0,0},{0,0},{0,0}});
+    }
     public double largestTriangleArea(int[][] p) 
     {
         int n=p.length;
         double max=0.0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n-2;i++)
         {
-            for(int j=i+1;j<n;j++)
+            for(int j=i+1;j<n-1;j++)
             {
                 for(int k=j+1;k<n;k++)
                 {
