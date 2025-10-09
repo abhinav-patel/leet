2ad516d1da290ac8157965c7c73685cb2efb768a
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2025-10-09 23:12:58 +0530

    Time: 434 ms (26.16%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,1 +1,1 @@
-select firstName,lastName,city,state from Person p left join Address A on p.personid=A.personId
+select p.firstName,p.lastName,A.city,A.state from Person as p left join Address as A on p.personid=A.personId
