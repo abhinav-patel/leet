932eea58baf284a640c3ea28d0dc8576ffc35747
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2025-10-20 11:02:22 +0530

    Time: 425 ms (37.76%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,1 +1,1 @@
-select distinct author_id  as id from views where author_id=viewer_id order by author_id asc;
+select author_id as id from views where author_id=viewer_id group by id order by author_id asc;
